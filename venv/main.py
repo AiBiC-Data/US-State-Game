a74fafc04f8950524d3f6c37ecc1effaacbebe03
AiBiC-Data <whrnjsgh12@naver.com>
2022-12-27 22:04:13 +0900

    make learn state csv file

--- venv/main.py
+++ venv/main.py
@@ -13,6 +13,14 @@ guessed_states =[]
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title =f"{len(guessed_states)}/50 States Correct", prompt = "What's another state's name?").title()
+    if answer_state == 'Exit':
+        missing_sates =[]
+        for state in all_states:
+            if state not in guessed_states:
+                missing_sates.append(state)
+        new_data = pandas.DataFrame(missing_sates)
+        new_data.to_csv("states_to_learn.csv")
+        break
     #if answer_sate is one of the states in all the states of the 50_states.csv
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -25,10 +33,13 @@ while len(guessed_states) < 50:
         #Create a turtle to write the name of the state at the state's x and y coordate
         t.write(answer_state)
 
+
+
+
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 # turtle.onscreenclick(get_mouse_click_coor)
 
-turtle.mainloop()
+# turtle.mainloop()
 
 # screen.exitonclick()
