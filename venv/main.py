c27a2530b7ba097fdd70b8097194069d2a477815
AiBiC-Data <whrnjsgh12@naver.com>
2022-12-27 21:50:54 +0900

    update popup title etc

--- venv/main.py
+++ venv/main.py
@@ -9,19 +9,21 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states =[]
 
-answer_state = screen.textinput(title ="Guess the State", prompt = "What's another state's name?")
-
-#if answer_sate is one of the states in all the states of the 50_states.csv
-if answer_state in all_states:
-    #if they got it right:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    #Create a turtle to write the name of the state at the state's x and y coordate
-    t.write(state_data.state.item())
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title =f"{len(guessed_states)}/50 States Correct", prompt = "What's another state's name?").title()
+    #if answer_sate is one of the states in all the states of the 50_states.csv
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        #if they got it right:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        #Create a turtle to write the name of the state at the state's x and y coordate
+        t.write(answer_state)
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
